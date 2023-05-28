d30e5f71eb6f1f733972bd6f7d2ec334409ff4bc
Mohammad Badier <>
2023-05-28 10:38:23 +0300

    create-angular-calculator-repo

--- Angular/src/app/calculator/calculator.component.ts
+++ Angular/src/app/calculator/calculator.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import { CalculatorService } from "./services/calculator.service";
 import { OperationTypes } from "./enums/operation-types.enum";
+import {FormBuilder, FormControl, FormGroup, UntypedFormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit{
   readonly operationTypes = OperationTypes;
-  selectedOperation: OperationTypes | null = null;
-  firstNumber: number = 0;
-  secondNumber: number = 0;
+  calculatorForm: FormGroup<{
+    selectedOperation: FormControl<OperationTypes>;
+    firstNumber: FormControl<number>;
+    secondNumber: FormControl<number>;
+  }>;
   result: number | null = null;
 
-  constructor(private calculatorService: CalculatorService) {}
+  constructor(private formBuilder: UntypedFormBuilder, private calculatorService: CalculatorService) {}
+
+
+  ngOnInit(): void {
+    this.calculatorForm = this.formBuilder.group({
+      selectedOperation: [OperationTypes.Add, [Validators.required]],
+      firstNumber: [0, [Validators.required]],
+      secondNumber: [0, [Validators.required]],
+    });
+  }
 
   calculate(): void {
-    switch (this.selectedOperation) {
+    const { selectedOperation, secondNumber, firstNumber} = this.calculatorForm.controls;
+    switch (selectedOperation.value) {
       case OperationTypes.Add:
-        this.result = this.calculatorService.getSum([this.firstNumber, this.secondNumber]);
+        this.result = this.calculatorService.getSum([firstNumber.value, secondNumber.value]);
         break;
       case OperationTypes.Multiply:
-        this.result = this.calculatorService.getMultiple([this.firstNumber, this.secondNumber]);
+        this.result = this.calculatorService.getMultiple([firstNumber.value, secondNumber.value]);
         break;
     }
   }
