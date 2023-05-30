29d0c1ff2ead51bde17e06b68969a4f5501b45d8
Or Menashe <or.m@next-insurance.com>
2023-05-31 01:40:56 +0300

    add react example tests

--- React/CalcApp/src/app/calculator/calculator.spec.tsx
+++ React/CalcApp/src/app/calculator/calculator.spec.tsx
@@ -3,8 +3,25 @@ import { render } from '@testing-library/react';
 import Calculator from './calculator';
 
 describe('Calculator', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Calculator />);
-    expect(baseElement).toBeTruthy();
-  });
+  describe('Component rendered', () => {
+    it('first number input should be default text', () => {
+      const expectedDefaultNumber = '0'
+      const {getByLabelText} = render(<Calculator />)
+
+      const comp = getByLabelText('first-input') as HTMLInputElement
+
+
+      expect(comp.value).toEqual(expectedDefaultNumber)
+    })
+
+    it('second number input should be default text', () => {
+      const expectedDefaultNumber = '0'
+      const {getByLabelText} = render(<Calculator />)
+
+      const comp = getByLabelText('second-input') as HTMLInputElement
+
+
+      expect(comp.value).toEqual(expectedDefaultNumber)
+    })
+  })
 });
--- React/CalcApp/src/app/calculator/calculator.tsx
+++ React/CalcApp/src/app/calculator/calculator.tsx
@@ -21,7 +21,7 @@ export function Calculator() {
   return (
     <div className={ styles.calculator }>
       <h2 className={ styles.calculator__title }>Calculator</h2>
-      
+
       <div className={ styles.calculator__operation }>
         <label className={ styles.calculator__radio_label }>
           <input type="radio" name="operation" value="add" checked={selectedOperation === 'add'} onChange={() => setSelectedOperation('add')} className={ styles.calculator__radio_input } /> Addition
@@ -30,19 +30,19 @@ export function Calculator() {
           <input type="radio" name="operation" value="multiply" checked={selectedOperation === 'multiply'} onChange={() => setSelectedOperation('multiply')} className={ styles.calculator__radio_input } /> Multiplication
         </label>
       </div>
-      
+
       <div className={ styles.calculator__input_group }>
         <label className={ styles.calculator__label }>First Number:</label>
-        <input type="number" value={firstNumber} onChange={e => setFirstNumber(Number(e.target.value))} className={ styles.calculator__input } />
+        <input aria-label="first-input"  type="number" value={firstNumber} onChange={e => setFirstNumber(Number(e.target.value))} className={ styles.calculator__input } />
       </div>
-      
+
       <div className={ styles.calculator__input_group }>
         <label className={ styles.calculator__label }>Second Number:</label>
-        <input type="number" value={secondNumber} onChange={e => setSecondNumber(Number(e.target.value))} className={ styles.calculator__input } />
+        <input aria-label="second-input"  type="number" value={secondNumber} onChange={e => setSecondNumber(Number(e.target.value))} className={ styles.calculator__input } />
       </div>
-      
+
       <button onClick={calculate} className={ styles.calculator__button }>Calculate</button>
-      
+
       {result !== null && (
         <div className={ styles.calculator__result }>
           <p className={ styles.calculator__result_text }>Result: {result}</p>
